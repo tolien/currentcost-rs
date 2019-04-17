a7a730bc18a8201251c0b7954a11eb8cde5bee51
Stuart Swindells <tolien@users.noreply.github.com>
2019-04-17 13:29:45 +0000

    Initial commit of Rust to load currentcost log into Postgres DB

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use clap::{Arg, App, Subcommand};
+
+use std::env;
+use std::process;
+
+use currentcost_store;
+use currentcost_store::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = currentcost_store::run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
