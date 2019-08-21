a9c78d7f244d4111e1403c44c16ddef8ffb90403
tolien <tolien@users.noreply.github.com>
2019-08-21 21:04:20 +0000

    Add a function to format a CurrentCostReading to be written to a log

--- src/reading.rs
+++ src/reading.rs
@@ -10,5 +10,35 @@ pub struct CurrentCostReading {
 }
 
 impl CurrentCostReading {
+    pub fn to_log(&self) -> String {
+        format!(
+            "{}, {}, Sensor {}, {:.2}°C, {}W\n",
+            self.timestamp.format("%d/%m/%Y %H:%M:%S"),
+            self.timestamp.timestamp(),
+            self.sensor,
+            self.temperature,
+            self.power
+        )
+    }
+}
 
+#[cfg(test)]
+mod tests {
+
+    use crate::reading::CurrentCostReading;
+    use chrono::prelude::*;
+
+    #[test]
+    fn convert_reading_to_log_line() {
+        let reading = CurrentCostReading {
+            timestamp: Utc.ymd(2019, 8, 20).and_hms(15, 40, 42),
+            device: String::from("CC128-v1.29"),
+            sensor: 0,
+            temperature: 24.8,
+            power: 3000,
+        };
+
+        let log_line = "20/08/2019 15:40:42, 1566315642, Sensor 0, 24.80°C, 3000W\n";
+        assert_eq!(reading.to_log(), log_line);
+    }
 }
